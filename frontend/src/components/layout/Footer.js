8bdfa4a41ee09ad9245a895799f58483ac37d508
webersoncavalari <weberson.cavalari1@gmail.com>
2023-08-01 21:44:20 -0300

    add pastas

---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return <footer className={styles.footer}>
+    <p><b>Get A Pet</b> &copy; 2023</p>
+  </footer>
+}
+
+export default Footer
---
+++ frontend/src/utils/bus.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/no-anonymous-default-export */
+import EventEmitter from "events";
+export default new EventEmitter();
